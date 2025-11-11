71d1c6fd360cf71dcfa6bc18aefb6124fc25945b
Anshika-S03 <anshikasharmar007@gmail.com>
2025-11-11 14:16:48 +0530

    chore: clean mongoose connection options

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,7 @@ app.use("/api/transactions", transactionRoutes);
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI);
   .then(() => console.log("MongoDB Connected Successfully"))
   .catch((err) => console.error("MongoDB Connection Error:", err));
 
